1739593a382e94e727a2ea108ace56e958fcb1ac
Geoffrey Lisch <geoffrey.lisch@gmail.com>
2024-01-29 08:35:06 -0600

    Code now checks for data in mongo and drops/replaces it if it exists.   Graphing added to output.

--- mongo.py
+++ mongo.py
@@ -1,10 +1,15 @@
 from pymongo import MongoClient
 import json 
-from stravalib.client import Client  
+from stravalib.client import Client
+from datetime import datetime  
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 with open('config.json', 'r') as f:
     cred = json.load(f)
-  
+
+# Replace the uri string with your MongoDB deployment's connection string.
 client_id = cred['strava']['client_id']
 client_secret = cred['strava']['client_secret']
 username = cred['mongo_cloud']['username']
@@ -35,7 +40,7 @@ def refresh_access_token(client_id, client_secret, refresh_token):
     return token_response['access_token']
 
 def oauth():
-    # Load existing tokens
+    # Load existing token
     access_token, refresh_token = load_tokens()
 
     if not access_token or not refresh_token:
@@ -52,10 +57,19 @@ def oauth():
 
     return access_token
 
-def get_activities(access_token):
+
+def get_activities(access_token, collection):
     client = Client(access_token=access_token)
-    activities = client.get_activities(limit=10)
+    activities = client.get_activities(limit = 1000)
     print('Strava Access Granted\nGrabbing activity details')  
+
+    db = mongoclient["strava"]    
+    collection = db["glisch"]
+    if collection.find_one():
+        print("Dropping Mongo collection for reload")
+        collection.drop()
+    else:
+        print('No existing collections found')
 
     for activity in activities:
         activity_data = {
@@ -63,23 +77,47 @@ def get_activities(access_token):
             "start_date": activity.start_date,
             "type": activity.type,
             "photos": activity.total_photo_count,
-        }
-        db = mongoclient["strava"]
+        }  
+        db = mongoclient["strava"]    
         collection = db["glisch"]
-        collection.insert_one(activity_data)
-    mongoclient.close()
+        collection.insert_one(activity_data)   #Insert data into MongoDB
     print('Activity details uploaded successfully')
+    
+def plot_data(collection, access_token):
+    # Get and upload activities to MongoDB if they don't exist
+    get_activities(access_token, collection)
 
-# Close the connection to MongoDB when you're done.
+    cursor = collection.find()
+    df = pd.DataFrame(list(cursor))
+    # Close the connection to MongoDB 
+    mongoclient.close()
+
+    print('Data moved from Mongo to Pandas')
+    df['start_date'] = pd.to_datetime(df['start_date'])
+
+    # Extract month and year from 'start_date'
+    df['month_year'] = df['start_date'].dt.to_period('M')
+
+    # Count activities per month and type
+    activities_per_month_type = df.groupby(['month_year', 'type']).size().unstack(fill_value=0)
+
+    activities_per_month_type.plot(kind='bar', stacked=True, figsize=(12, 8), colormap='viridis')
+    plt.title('Activities per Month Separated by Type')
+    plt.xlabel('Month-Year')
+    plt.ylabel('Number of Activities')
+    plt.legend(title='Activity Type', bbox_to_anchor=(1.05, 1), loc='upper left')
+    plt.show()
+
 
 def main():
     # Get access token (refresh if necessary)
     access_token = oauth()
 
-    # Get and upload activities to MongoDB
-    get_activities(access_token)
+    db = mongoclient["strava"]
+    collection = db["glisch"]
 
+    plot_data(collection, access_token)
+
+     
 if __name__ == "__main__":
     main()
-
-
